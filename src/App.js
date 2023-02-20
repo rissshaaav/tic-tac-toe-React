47b02cb82c4eb530ea1621753114fc82ae37a6dc
Rishav <111266613+rissshaaav@users.noreply.github.com>
2023-02-20 14:38:46 +0530

    created main screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import MainScreen from './Components/MainScreen/MainScreen';
 
 function App() {
   return (
     <div className="App">
-
+      <MainScreen/>
     </div>
   );
 }
---
+++ src/Components/MainScreen/MainScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './MainScreenStyle.css'
+import {AiOutlinePlayCircle} from 'react-icons/ai'
+import {FaStar, FaShareAlt, FaVolumeUp} from 'react-icons/fa'
+
+const MainScreen = () => {
+  return (
+    <div className='mainScreen'>
+        <header className='label'>
+            <div className='label-letter label-item'><span className='label-letter-item'>T</span></div>
+            <div className='label-letter label-item'><span className='label-letter-item'>i</span></div>
+            <div className='label-letter label-item'><span className='label-letter-item'>c</span></div>
+            <div className='label-letter label-item'><span className='label-letter-item'>T</span></div>
+            <div className='label-letter label-item'><span className='label-letter-item'>a</span></div>
+            <div className='label-letter label-item'><span className='label-letter-item'>c</span></div>
+            <div className='toe label-item'><span className='label-letter-item'>Toe</span></div>
+        </header>
+
+        <main>
+            <AiOutlinePlayCircle size='120px' id='playButton'/>
+        </main>
+
+        <footer>
+            <div className='footer-items'>
+                <FaVolumeUp size='40px' className='footer-icons' id='volume'/>
+            </div>
+            <div className='footer-items'>
+                <FaStar size='40px' className='footer-icons' id='rate'/>
+            </div>
+            <div className='footer-items'>
+                <FaShareAlt size='40px' className='footer-icons' id='share'/>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default MainScreen
